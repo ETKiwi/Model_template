5c935125ece9e1745a28dc3c980841905e2e3cb0
Bill <63846041+ETKiwi@users.noreply.github.com>
2022-12-14 17:55:24 +0800

    [新增]所有資料

---
+++ ML/pipeline/steps/preflight.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 14 11:29:24 2022
+
+@author: BK
+"""
+# import os
+# import glob
+def process(self, data, inputs, utils):
+    print('in Preflight')
+
+
+
+
